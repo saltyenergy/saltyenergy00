47db903d67207324be3a7634cee8d83bf02f7e7a
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-10-25 11:21:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saltyenergy::Application.config.secret_token = '46be407bf0cd1b057bc305377a8acd17dfed7af42ace62f685d2736bc415acbb0febd88cd94db0826dc06bb999f19d6e7aca4f2454a569e58b60a2ec727d21dd'
